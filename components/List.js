da5b0c20d11fa7d4b857a2fa3e9c1484b7583d96
momras <mohammra@metropolia.fi>
2023-01-14 16:59:51 +0200

    this is http-b

---
+++ components/List.js
@@ -0,0 +1,35 @@
+import {useEffect, useState} from 'react';
+import {FlatList, StyleSheet} from 'react-native';
+import ListItem from './ListItem';
+
+const List = () => {
+  const url =
+    'https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json';
+  const [mediaArray, setMediaArray] = useState([]);
+
+  const loadMedia = async () => {
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      setMediaArray(json);
+    } catch (error) {
+      console.error('List, loadMedia', error);
+    }
+  };
+
+  useEffect(() => {
+    loadMedia();
+  }, []);
+
+  console.log('List, mediaArray', mediaArray);
+
+  return (
+    <FlatList
+      data={mediaArray}
+      keyExtractor={(item, index) => index.toString()}
+      renderItem={({item}) => <ListItem singleMedia={item} />}
+    />
+  );
+};
+
+export default List;
---
+++ components/ListItem.js
@@ -0,0 +1,25 @@
+import {Image, StyleSheet, View, Text, TouchableOpacity} from 'react-native';
+
+import PropTypes from 'prop-types';
+
+const ListItem = ({singleMedia}) => {
+  const item = singleMedia;
+  return (
+    <TouchableOpacity>
+      <Image
+        style={{width: 100, height: 100}}
+        source={{uri: item.thumbnails.w160}}
+      ></Image>
+      <view>
+        <Text>{item.title}</Text>
+        <Text>{item.description}</Text>
+      </view>
+    </TouchableOpacity>
+  );
+};
+
+ListItem.PropTypes = {
+  singleMedia: PropTypes.object,
+};
+
+export default ListItem;
