90e7c0739f81f5e605afca3f4ed983c311881fe6
momras <mohammra@metropolia.fi>
2023-01-16 04:02:20 +0200

    App.js is updates

--- App.js
+++ App.js
@@ -1,26 +1,13 @@
 import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, SafeAreaView} from 'react-native';
-import List from './components/List';
+import Navigator from './navigators/Navigator';
 
 const App = () => {
-  //console.log('App starting!')
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <List />
-      </SafeAreaView>
+      <Navigator />
       <StatusBar style="auto" />
     </>
   );
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
